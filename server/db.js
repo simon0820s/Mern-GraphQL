0305a3da228b3a611fc295a35d33ce82507e7abe
simon0820s <simon0820s@gmail.com>
2023-08-09 12:35:20 -0500

    connect db

--- server/db.js
+++ server/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import {MONGODB_URI} from './config.js'
 
 export const connectDb = async () => {
   try {
 
-  const conn = await mongoose.connect("mongodb://localhost/merndb");
+  const conn = await mongoose.connect(MONGODB_URI);
   console.log(`Mongodb connected: ${conn.connection.name}`)
   } catch (error) {
     console.error(`Error: ${error.message}`)
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import { startApolloServer } from "./app.js";
 import {typeDefs} from './graphql/typeDef.js'
 import {resolvers} from './graphql/resolvers.js'
+import { connectDb } from "./db.js";
+
+connectDb()
 
 startApolloServer(typeDefs, resolvers);
