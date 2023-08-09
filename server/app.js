62e16ff803bad34767163c767fd436ca1ef66f9d
simon0820s <simon0820s@gmail.com>
2023-08-09 11:48:42 -0500

    dev mode activate

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
-import http from 'http'
+import http from "http";
 
 export async function startApolloServer(typeDefs, resolvers) {
   const app = express();
   const httpServer = http.createServer(app);
+
+  app.get('/', (req, res) => res.send('Welcome to my api'));
 
   const gServer = new ApolloServer({
     typeDefs,
@@ -17,11 +19,16 @@ export async function startApolloServer(typeDefs, resolvers) {
 
   app.use("/graphql", cors(), express.json(), expressMiddleware(gServer));
 
-  await new Promise(resolve => httpServer.listen({
-    port: 3000
-  }, resolve))
+  await new Promise((resolve) =>
+    httpServer.listen(
+      {
+        port: 3000,
+      },
+      resolve
+    )
+  );
 
-  console.log("Server ready at http://localhost:3000")
+  console.log("Server ready at http://localhost:3000");
 
   return app;
 }
