5eedd71f40a42297dba974db15be9494f549d7d6
simon0820s <simon0820s@gmail.com>
2023-08-10 21:00:31 -0500

    create model Project schema

---
+++ server/models/Project.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model('Project', ProjectSchema)
