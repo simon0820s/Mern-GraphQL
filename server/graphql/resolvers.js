bcf43c9494f2ab87cc212eee5c820f02f47410ce
simon0820s <simon0820s@gmail.com>
2023-08-15 21:31:11 -0500

    create delete function

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,14 +1,13 @@
 import Project from "../models/Project.js";
-import Task from "../models/Task.js"
+import Task from "../models/Task.js";
 
 export const resolvers = {
   Query: {
     hello: () => "Hello World",
     projects: async () => await Project.find(),
-    project: async (_, {_id}) => await Project.findById(_id),
-    task: async (_, {_id}) => await Task.findById(_id),
-    tasks: async () => await Task.find()
-
+    project: async (_, { _id }) => await Project.findById(_id),
+    task: async (_, { _id }) => await Task.findById(_id),
+    tasks: async () => await Task.find(),
   },
   Mutation: {
     createProject: async (_, { name, description }) => {
@@ -19,17 +18,25 @@ export const resolvers = {
       const savedProject = await project.save();
       return savedProject;
     },
-    createTask: async (_, {title, projectId}) => {
+    createTask: async (_, { title, projectId }) => {
+      const projectFound = await Project.findById(projectId);
 
-      const projectFound = await Project.findById(projectId)
-
-      if (!projectFound) throw new Error('Project not found')
+      if (!projectFound) throw new Error("Project not found");
 
       const task = new Task({
         title,
-        projectId
-      })
-      return await task.save()
+        projectId,
+      });
+      return await task.save();
+    },
+    deleteProject: async (_, { _id }) => {
+      const deletedProject = await Project.findByIdAndDelete(_id);
+      if (!deletedProject) throw new Error("Project not found");
+      return deletedProject;
+    },
+    deleteTask: async(_, { _id }) => {
+      const deletedTask = await Project.findByIdAndDelete(_id)
+      if(!deletedTask) throw new Error("Task not found")
     }
   },
 };
